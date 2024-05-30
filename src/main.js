1dc7b0ea5dd9dc9de4c645f9a7d454e1a973589d
Odesii <alic.madd@gmail.com>
2024-05-29 21:23:54 -0400

    assets for world and two characters

--- src/main.js
+++ src/main.js
@@ -1,5 +1,36 @@
 import Phaser from './lib/phaser.js'
+import { SCENE_KEYS } from './scenes/sceneKey.js';
+import { PreloadScene } from './scenes/preloadScene.js';
+import { WorldScene } from './scenes/worldScene.js';
 
-const game = new Phaser.Game({
+const config = {
+    type: Phaser.AUTO,
+    width: window.innerWidth,
+    height: window.innerHeight,
+    scene: [WorldScene],
+    scale: {
+        mode: Phaser.Scale.RESIZE,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: false
+        }
+    },
     parent: 'game-container',
+    scene:[
+        WorldScene,
+    ]
+};
+
+const game = new Phaser.Game(config);
+
+window.addEventListener('resize', () => {
+    game.scale.resize(window.innerWidth, window.innerHeight);
 });
+
+//these are for manually adding the scene and starting 
+// game.scene.add(SCENE_KEYS.PRELOAD_SCENE, PreloadScene)
+// game.scene.start(SCENE_KEYS.PRELOAD_SCENE);
+
---
+++ src/scenes/preloadScene.js
@@ -0,0 +1,49 @@
+import Phaser from "../lib/phaser.js";
+import { SCENE_KEYS } from "./sceneKey.js";
+
+export class PreloadScene extends Phaser.Scene {
+    constructor(){
+        super({
+            key: SCENE_KEYS.PRELOAD_SCENE,
+            active: true
+        });
+        console.log(SCENE_KEYS.PRELOAD_SCENE);
+    }
+
+    // init(){
+    //     console.log('init');
+    // }
+
+    preload(){
+        // Load the tileset images
+        this.load.image('Minifantasy_ForgottenPlainsTiles', '../../assets/map/Minifantasy_ForgottenPlainsTiles.png');
+        this.load.image('OutdoorTileset', '../../assets/map/OutdoorTileset.png');
+
+        // Load the tilemap JSON file
+        this.load.tilemapTiledJSON('map', '../../assets/map/Map.tmj');
+    }
+
+    create(){
+        // Create the tilemap
+        const map = this.make.tilemap({ key: 'map' });
+
+        // Add the tilesets to the map (ensure the names match those used in Tiled)
+        const tiles = map.addTilesetImage('Minifantasy_ForgottenPlainsTiles', 'Minifantasy_ForgottenPlainsTiles');
+        const tiles1 = map.addTilesetImage('OutdoorTileset', 'OutdoorTileset');
+
+        // Create layers from the tilemap (ensure the layer names match those in Tiled)
+        const baseLayer = map.createLayer('base', tiles);
+        const waterLayer = map.createLayer('water', tiles);
+        const roadLayer = map.createLayer('roads', tiles);
+        const underLayer = map.createLayer('under_build', tiles);
+        const buildingLayer = map.createLayer('buildings', tiles);
+        const castleLayer = map.createLayer('Castle_wall', tiles1);
+        const doorLayer = map.createLayer('Castle_Door', tiles1);
+
+        console.log('create');
+    }
+
+    update(){
+        console.log('update');
+    }
+}
